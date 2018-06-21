d16634ffcbd27bbf6fd374d457cb10aa46367b41
Alex Demskie <alex.demskie@bigfishgames.com>
2018-06-20 23:40:39 -0700

    adding benchmarks

--- README.md
+++ README.md
@@ -12,7 +12,12 @@ demskie$ go test github.com/demskie/subnetmath -bench=.
 goos: darwin
 goarch: amd64
 pkg: github.com/demskie/subnetmath
-
-TODO
-
+BenchmarkNetworkComesBefore-8   	50000000	        26.9 ns/op
+BenchmarkGetClassfulNetwork-8   	20000000	        87.6 ns/op
+BenchmarkAddToAddr-8            	300000000	         5.11 ns/op
+BenchmarkNextAddr-8             	50000000	        29.3 ns/op
+BenchmarkShrinkNetwork-8        	 1000000	      1691 ns/op
+BenchmarkNextNetwork-8          	 5000000	       286 ns/op
+BenchmarkGetAllAddresses-8      	   30000	     44225 ns/op
+BenchmarkFindUnusedSubnets-8    	    5000	    330295 ns/op
 ```
--- subnetmath_test.go
+++ subnetmath_test.go
@@ -31,3 +31,67 @@ func TestUnusedSubnets(t *testing.T) {
 		)
 	}
 }
+
+func BenchmarkNetworkComesBefore(b *testing.B) {
+	network := BlindlyParseCIDR("192.168.0.0/28")
+	otherNetwork := BlindlyParseCIDR("192.168.0.0/22")
+	for i := 0; i < b.N; i++ {
+		NetworkComesBefore(network, otherNetwork)
+	}
+}
+
+func BenchmarkGetClassfulNetwork(b *testing.B) {
+	addr := net.ParseIP("192.168.0.0")
+	for i := 0; i < b.N; i++ {
+		GetClassfulNetwork(addr)
+	}
+}
+
+func BenchmarkAddToAddr(b *testing.B) {
+	addr := net.ParseIP("192.168.0.0")
+	for i := 0; i < b.N; i++ {
+		addr = AddToAddr(addr, 1)
+	}
+}
+
+func BenchmarkNextAddr(b *testing.B) {
+	addr := net.ParseIP("192.168.0.0")
+	for i := 0; i < b.N; i++ {
+		addr = NextAddr(addr)
+	}
+}
+
+func BenchmarkShrinkNetwork(b *testing.B) {
+	startNetwork := BlindlyParseCIDR("8.0.0.0/8")
+	for i := 0; i < b.N; i++ {
+		network := DuplicateNetwork(startNetwork)
+		for j := 0; j < 24; j++ {
+			network = ShrinkNetwork(network)
+		}
+	}
+}
+
+func BenchmarkNextNetwork(b *testing.B) {
+	network := BlindlyParseCIDR("192.168.0.0/28")
+	for i := 0; i < b.N; i++ {
+		network = NextNetwork(network)
+	}
+}
+
+func BenchmarkGetAllAddresses(b *testing.B) {
+	network := BlindlyParseCIDR("192.168.0.0/22")
+	for i := 0; i < b.N; i++ {
+		GetAllAddresses(network)
+	}
+}
+
+func BenchmarkFindUnusedSubnets(b *testing.B) {
+	aggregate := BlindlyParseCIDR("192.168.0.0/22")
+	subnets := []*net.IPNet{
+		BlindlyParseCIDR("192.168.1.0/24"),
+		BlindlyParseCIDR("192.168.2.32/30"),
+	}
+	for i := 0; i < b.N; i++ {
+		UnusedSubnets(aggregate, subnets...)
+	}
+}
